9bb2ff6c68f6f8eb5acec1a0193b618537fda18b
phanau <aupv96@gmail.com>
2019-07-15 10:51:56 +0700

    react update

---
+++ src/pages/home/homeleft/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from '../img/logo.png';
+
+
+
+class HomeLeft extends Component {
+    render() {
+        return (
+            <img src={logo} id="logo"></img>
+        );
+    }
+}
+
+export default HomeLeft;
---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HomeLeft from './homeleft';
+
+class Home extends Component {
+    render() {
+        return (
+           <div id="home">
+               <HomeLeft></HomeLeft>
+           </div>
+        );
+    }
+}
+
+export default Home;
